130b44677fe8de032070e0c7a6826f7d282f8725
BlackG04T <zetinaluis03@gmail.com>
2022-10-20 19:10:30 -0500

    correcciones y cambios de estilos

--- Parcial2/SideBarJS/sidebarjs.js
+++ Parcial2/SideBarJS/sidebarjs.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     document.getElementById("botonMenu").addEventListener("click",funcionBoton);
     function funcionBoton(){
-        document.getElementById("IDsidebar").classList.toggle("hidden")
+        document.getElementById("IDsidebar").classList.toggle("escondido")
         console.log("click")
     }
 }
